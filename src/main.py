29f79f05b7bc3cfc5285e6db9d639888d08ad9d6
Cameron James <cjames0815@gmail.com>
2023-11-10 10:44:14 -0500

    First Version of Stack

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 from node.node import *
+from stack.stack import *
+
 
 def main():
     #testInit()
@@ -14,7 +16,35 @@ def main():
     #testListCopy()
     #testListCopyWithTail()
     #testreview()
-    assignment()
+    #assignment()
+    testPush()
+
+    s = stack()
+    print("Stack size is:", s.size()) # 0
+    print("Stack contains:", s)       # []
+
+    s.push('S')
+    print("Stack size is:", s.size()) # 1
+    print("Stack contains:", s)       # [S]
+
+    #s.push('B')
+    s.push(1)
+    print("Stack size is:", s.size()) # 2
+    print("Stack contains:", s)       # [B S]
+
+    #s.push('O')
+    s.push((1,2))
+    print("Stack size is:", s.size()) # 3
+    print("Stack contains:", s)       # [O B S]
+
+    #s.push('J')
+    s.push([1,2,3])
+    print("Stack size is:", s.size()) # 4
+    print("Stack contains:", s)       # [J O B S]
+
+
+def testPush():
+    print("Testing Push Method in Stack Class")
 
 def assignment():
 
---
+++ src/stack/stack.py
@@ -0,0 +1,103 @@
+from node.node import *
+
+class stack:
+    """Stack class that uses linked lists of nodes as its underlying data structure. 
+    """
+    def __init__(self):
+        """Constructs an empty stack.
+        """        
+        self.__head = None          #reference to the top node in the stack
+        self.__tail = self.__head   #reference to the bottom node in the stack 
+        self.__manyNodes = 0          #keeps track of the number of nodes in the stack
+    
+    def size(self):
+
+        """Returns the number of nodes in the calling stack
+
+        Returns:
+        int: number of nodes
+        """ 
+        return self.__manyNodes
+
+    def getHead(self):
+        """Returns a reference to the heaf (top) of the calling stack
+        
+        Returns:
+            node:reference to the head (top) of the calling stack
+        """
+        return self.__head
+
+    def getTail(self):
+        """Reutrns a reference to the tail(bottom)of the calling stack
+
+        Returns:
+            node: reference to the tail (bottom) of the calling stack
+        """        
+        return self.__tail
+    
+    def getData(self):
+        """Returns the data values in the calling stack.
+
+        Returns:
+            _type_: Returns the data values in the calling stack.
+        """        
+        cursor = self.__head    # used to step through the nodes in the calling stack
+        data = ""               # string representation of data values in the calling stack
+        i = 1                   # used to count the nodes in the calling stack
+
+        # loop through the calling stack, one node at a time , getting the data values
+        # and concatenating them to data
+        while(i <= self.__manyNodes):
+            data = data + (str(cursor.getData()) + ' ')
+            cursor = cursor.getLink()
+            i += 1
+
+        # return data
+        return data
+
+    def __str__(self):
+        """Returns string representation of the calling stack
+
+        Returns:
+            str: string representation of the calling stack
+        """        
+        return f"[{self.getData()}]"
+    
+    def push(self, element):
+        """Pushes (adds) the specified element to the top of the calling stack
+
+
+        Args:
+            element specified element
+        """
+       # if the calling stack is empty
+        if (self.__head == None):
+            #add node tp ca;;ing stack
+            self.__head = node(element, None)
+            # make tailrefer to the same node as head
+            self.__tail = self.__head
+        else:
+            # add node to the top of the stack
+            self.__head = node(element, self.__head)
+
+        #get the number of nodes in the calling stack
+        self.__manyNodes = node.listlength(self.__head)  
+
+    def isEmpty(self):
+        """Checks if the calling stack is empty
+
+        Returns:
+            Boolean: True if the calling stack is empty, e;se False
+        """        
+        return self.size() == 0 
+
+    def pop(self):
+        """Removes and returns the element at the head (top) of the calling stack
+
+        Raises:
+            ValueError: indicates calling stack is empty
+        
+        Returns:
+            _types_: element at the top of the calling stack
+        
+        """           
